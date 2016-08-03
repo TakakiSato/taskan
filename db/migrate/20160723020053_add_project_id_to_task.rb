3920b382751df30bd2902da6ab39e724cd0cd842
TakakiSato <ruby@vagrant-centos64.vagrantup.com>
2016-08-03 14:07:33 +0000

    task type commit add

--- db/migrate/20160723020053_add_project_id_to_task.rb
+++ db/migrate/20160723020053_add_project_id_to_task.rb
@@ -1,8 +1,15 @@
 class AddProjectIdToTask < ActiveRecord::Migration
-  def change
+  def up
     change_table :tasks do |t|
         t.references :project
         t.references :task_type
     end
 end
+def down
+    change_table :tasks do |t|
+        t.references :project
+        t.references :task_type
+    end
+end
+
 end
--- db/migrate/20160731023741_add_project_id_to_task_type.rb
+++ db/migrate/20160731023741_add_project_id_to_task_type.rb
@@ -1,10 +1,18 @@
 class AddProjectIdToTaskType < ActiveRecord::Migration
-  def change
+  def up
     change_table :task_types,id: false  do |t|
         t.references :project
     end
     change_column :tasks,   :plan_time ,:float
     change_column :tasks,   :result_time ,:float
-
   end
+
+  def down
+    change_table :task_types,id: false  do |t|
+        t.references :project
+    end
+    change_column :tasks,   :plan_time ,:float
+    change_column :tasks,   :result_time ,:float
+  end
+
 end
